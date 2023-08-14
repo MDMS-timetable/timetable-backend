356a2c0bbef2d7a1b9cb6df4c44464f2ed1c397b
anys34 <anyeseong34@gmail.com>
2023-08-14 23:02:34 +0900

    ADD :: 데이터 요청

--- models/index.js
+++ models/index.js
@@ -4,6 +4,8 @@ const config = require("../config/config")[env];
 const db = {};
 
 const User = require("./user");
+const Hits_Timetable = require("./hits_timetable");
+const Hits_Lunch = require("./hits_lunch");
 
 const sequelize = new Sequelize(
   config.database,
@@ -13,10 +15,16 @@ const sequelize = new Sequelize(
 );
 
 db.User = User;
+db.User = Hits_Timetable;
+db.User = Hits_Lunch;
 
 User.initiate(sequelize);
+Hits_Timetable.initiate(sequelize);
+Hits_Lunch.initiate(sequelize);
 
 User.associate(db);
+Hits_Timetable.associate(db);
+Hits_Lunch.associate(db);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
--- routes/lunch.js
+++ routes/lunch.js
@@ -3,31 +3,43 @@ const router = express.Router();
 const axios = require("axios");
 
 router.post("/mealinfo", async (req, res) => {
-  month = req.body.month;
-  try {
-    const response = await axios.get(
-      `https://open.neis.go.kr/hub/mealServiceDietInfo?Key=080d50080aa344b8ac686028b5e2a1e2&Type=json&ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7201023&MLSV_YMD=${month}`
-    );
-    const rows = response.data.mealServiceDietInfo[1].row;
-    let mealsByDate = [];
-
-    rows.forEach((row) => {
-      const cleanedDDISH_NM = row.DDISH_NM
-        .replace(/\([^()]*\)/g, '')
-        .replace(/<br\/>/g, '')
-        .replace(/\s{2,}/g, ' ')
-        .trim();
-      
-      mealsByDate.push({
-        lunch: cleanedDDISH_NM,
-        calorie: row.CAL_INFO,
-        date: row.MLSV_FROM_YMD
-      });
-    });
-    res.send(mealsByDate);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching meal information" });
+  date = req.body.date;
+  start = req.body.start;
+  end = req.body.end;
+  let response;
+  if (date.length > 0) {
+    try {
+      response = await axios.get(
+        `https://open.neis.go.kr/hub/mealServiceDietInfo?Key=080d50080aa344b8ac686028b5e2a1e2&Type=json&ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7201023&MLSV_YMD=${date}`
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching meal information" });
+    }
+  } else {
+    try {
+      response = await axios.get(
+        `https://open.neis.go.kr/hub/mealServiceDietInfo?Key=080d50080aa344b8ac686028b5e2a1e2&Type=json&ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7201023&MLSV_FROM_YMD=${start}&MLSV_TO_YMD=${end}`
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching meal information" });
+    }
   }
+  const rows = response.data.mealServiceDietInfo[1].row;
+  let mealsByDate = [];
+
+  rows.forEach((row) => {
+    const cleanedDDISH_NM = row.DDISH_NM.replace(/\([^()]*\)/g, "")
+      .replace(/<br\/>/g, "")
+      .replace(/\s{2,}/g, " ")
+      .trim();
+
+    mealsByDate.push({
+      lunch: cleanedDDISH_NM,
+      calorie: row.CAL_INFO,
+      date: row.MLSV_FROM_YMD,
+    });
+  });
+  res.send(mealsByDate);
 });
 
 module.exports = router;

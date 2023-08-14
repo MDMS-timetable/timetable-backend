e37b9210f892d01dd6d916cd20922b9c67c788b2
anys34 <anyeseong34@gmail.com>
2023-08-15 00:15:38 +0900

    FIX :: rows

--- routes/lunch.js
+++ routes/lunch.js
@@ -24,33 +24,38 @@ router.post("/mealinfo", async (req, res) => {
       res.status(500).json({ message: "Error fetching meal information" });
     }
   }
-  const rows = response.data.mealServiceDietInfo[1].row;
-  let mealsByDate = [];
 
-  rows.forEach((row) => {
-    const cleanedDDISH_NM = row.DDISH_NM.replace(/\([^()]*\)/g, "")
-      .replace(/<br\/>/g, "")
-      .replace(/\s{2,}/g, " ")
-      .trim();
+  if (!response.data || !response.data.mealServiceDietInfo || response.data.mealServiceDietInfo.length < 2) {
+    res.status(500).json({ message: "Error fetching meal information" });
+  } else {
+    const rows = response.data.mealServiceDietInfo[1].row;
+    let mealsByDate = [];
 
-    const dateParts = row.MLSV_FROM_YMD.match(/(\d{4})(\d{2})(\d{2})/);
+    rows.forEach((row) => {
+      const cleanedDDISH_NM = row.DDISH_NM.replace(/\([^()]*\)/g, "")
+        .replace(/<br\/>/g, "")
+        .replace(/\s{2,}/g, " ")
+        .trim();
 
-    const formatWithLeadingZero = (number) => {
-      const strNumber = number.toString();
-      return strNumber.length === 1 ? "0" + strNumber : strNumber;
-    };
+      const dateParts = row.MLSV_FROM_YMD.match(/(\d{4})(\d{2})(\d{2})/);
 
-    mealsByDate.push({
-      lunch: cleanedDDISH_NM,
-      calorie: row.CAL_INFO,
-      date: {
-        year: parseInt(dateParts[1]),
-        month: formatWithLeadingZero(parseInt(dateParts[2])),
-        day: formatWithLeadingZero(parseInt(dateParts[3])),
-      },
+      const formatWithLeadingZero = (number) => {
+        const strNumber = number.toString();
+        return strNumber.length === 1 ? "0" + strNumber : strNumber;
+      };
+
+      mealsByDate.push({
+        lunch: cleanedDDISH_NM,
+        calorie: row.CAL_INFO,
+        date: {
+          year: parseInt(dateParts[1]),
+          month: formatWithLeadingZero(parseInt(dateParts[2])),
+          day: formatWithLeadingZero(parseInt(dateParts[3])),
+        },
+      });
     });
-  });
-  res.send(mealsByDate);
+    res.send(mealsByDate);
+  }
 });
 
 module.exports = router;
